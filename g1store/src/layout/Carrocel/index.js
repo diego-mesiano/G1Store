9dd4176d47736cc5b94b7ca055cf844f9a41a642
William Lima <wilima010@gmail.com>
2021-12-08 11:56:47 -0300

    cards alinhados

--- g1store/src/layout/Carrocel/index.js
+++ g1store/src/layout/Carrocel/index.js
@@ -21,33 +21,34 @@ function Carrocel() {
 
     return (
         <>
+            <div classname="container">
+                <Carousel variant="dark" id="carrocel">
+                    <Carousel.Item interval={3000}>
 
-            <Carousel variant="dark" id="carrocel">
-                <Carousel.Item interval={3000}>
+                        <img
+                            id="carrocel"
+                            className="d-block w-100 justify-content-center"
+                            src="https://static.vecteezy.com/ti/vetor-gratis/p1/2303937-abstrato-cinza-fundo-com-linhas-onduladas-vetor.jpg"
+                            alt="imagem de fundo do carrocel"
+                        />
+                        <Carousel.Caption id="texto-carrocel">
+                            <GeraCards inicio={0} fim={3} produto={produtos}/>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={3000}>
 
-                    <img
-                        id="carrocel"
-                        className="d-block w-100 justify-content-center"
-                        src="https://static.vecteezy.com/ti/vetor-gratis/p1/2303937-abstrato-cinza-fundo-com-linhas-onduladas-vetor.jpg"
-                        alt="imagem de fundo do carrocel"
-                    />
-                    <Carousel.Caption id="texto-carrocel">
-                        <GeraCards inicio={0} fim={3} produto={produtos}/>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={3000}>
-
-                    <img
-                        id="carrocel"
-                        className="d-block w-100"
-                        src="https://static.vecteezy.com/ti/vetor-gratis/p1/2303937-abstrato-cinza-fundo-com-linhas-onduladas-vetor.jpg"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption >
-                    <GeraCards inicio={3} fim={6} produto={produtos}/>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                        <img
+                            id="carrocel"
+                            className="d-block w-100"
+                            src="https://static.vecteezy.com/ti/vetor-gratis/p1/2303937-abstrato-cinza-fundo-com-linhas-onduladas-vetor.jpg"
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption >
+                        <GeraCards inicio={3} fim={6} produto={produtos}/>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </>
     );
 }
