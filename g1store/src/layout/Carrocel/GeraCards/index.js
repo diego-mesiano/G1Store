ad077fd5eef2988dfd0fdbaeb42860f104082d1a
diego-mesiano <diego.mesiano1@gmail.com>
2021-12-08 11:58:16 -0300

    Ajustes na pagina de produtos gerais

--- g1store/src/layout/Carrocel/GeraCards/index.js
+++ g1store/src/layout/Carrocel/GeraCards/index.js
@@ -1,28 +1,33 @@
+import { Button } from "react-bootstrap";
+const GeraCards = ({ inicio, fim, produto }) => {
 
-const GeraCards = ({inicio, fim, produto}) => {
-        
     return (
         <>
-        <h2 id="titulo carrocel">Produtos em Destaque</h2>
-        <div id="destaques-container">
-        {   
-            produto.slice(inicio, fim).map((produto) => {
-                
-                return (
-                    
-                    <ul key={produto.id}>
-                            <img src={produto.imagem} alt={produto.nome} />
-                            <li><h3>{produto.nome}</h3></li>
-                            <li><h6>{produto.preco}</h6></li>
-                            <li>ou 10x R${produto.preco / 10},00</li>
-                        </ul>
+            <h2 id="titulo carrocel">Produtos em Destaque</h2>
+            <div id="destaques-container">
+                {
+                    produto.slice(inicio, fim).map((produto) => {
 
-                    
+                        return (
+                            <>
+                                <ul key={produto.id}>
+                                    <img src={produto.imagem} alt={produto.nome} />
+                                    <li><h3>{produto.nome}</h3></li>
+                                    <li><h6>{produto.preco}</h6></li>
+                                    <li>ou 10x R${produto.preco / 10},00</li>
+                                </ul>
+                                <div className="div-botoes">
 
-                )
-            })           
-        }
-        </div>    
+                                    <Button size="md" id="botoes-produto">Adicionar Carrinho</Button>
+                                    <Button size="md" id="botoes-produto" disabled>Comprar</Button>
+
+                                </div>
+                            </>
+                        )
+                    })
+                }
+
+            </div>
         </>
     )
 }
--- g1store/src/layout/ProdutoMain/index.js
+++ g1store/src/layout/ProdutoMain/index.js
@@ -2,11 +2,12 @@ import './style.scss';
 import { useParams } from 'react-router';
 import apiProdutos from '../../services/apiProdutos';
 import { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
 
 
 
 function ProdutoMain() {
-  
+
   const { produtoId } = useParams();
   const [produto, setProduto] = useState({});
   useEffect(() => {
@@ -19,12 +20,22 @@ function ProdutoMain() {
   }, [produtoId])
   return (
     <>
-    <div id="teste">
-      <p>{produto.nome}</p>
-      <p>{produto.preco}</p>
-      <p>{produto.descricao}</p>
-    </div>
-      
+      <main className="corpo-produto">
+        <section className="imagem-section">
+          <img className="imagem-produto" src={produto.imagem} alt={produto.nome} />
+        </section>
+        <section className="imagem-section">
+          <h2>{produto.nome}</h2>
+          <p>R${produto.preco}</p>
+          <p>{produto.descricao}</p>
+          <div className="div-botoes">
+            <Button size="md" id="botoes-produto">Adicionar Carrinho</Button>
+            <Button size="md" id="botoes-produto" disabled>Comprar</Button>
+          </div>
+
+        </section>
+      </main>
+
 
       {/* {<Header/>
             <ProdutoMain/>
