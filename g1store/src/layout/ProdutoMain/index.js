08b053d6625f7655ddb70a2e1c6939e95da98270
diego-mesiano <diego.mesiano1@gmail.com>
2021-12-08 10:25:00 -0300

    iniciei criação da pagina de produto especifico

--- g1store/src/layout/ProdutoMain/index.js
+++ g1store/src/layout/ProdutoMain/index.js
@@ -1,11 +1,37 @@
 import './style.scss';
+import { useParams } from 'react-router';
+import apiProdutos from '../../services/apiProdutos';
+import { useState, useEffect } from 'react'
+
+
 
 function ProdutoMain() {
-    
+  
+  const { produtoId } = useParams();
+  const [produto, setProduto] = useState({});
+  useEffect(() => {
+    apiProdutos
+      .get(`/produto/${produtoId}`)
+      .then((response) => setProduto(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      })
+  }, [produto])
   return (
-      <>
-      </>
+    <>
+    <div id="teste">
+      <p>{produto.nome}</p>
+      <p>{produto.preco}</p>
+      <p>{produto.descricao}</p>
+    </div>
+      
+
+      {/* {<Header/>
+            <ProdutoMain/>
+            <Footer/>} */}
+    </>
   );
+
 }
 
 export default ProdutoMain;
--- g1store/src/pages/Produto/index.js
+++ g1store/src/pages/Produto/index.js
@@ -1,32 +1,15 @@
-// import Header from '../../layout/Header/Header';
-// import ProdutoMain from '../../layout/ProdutoMain'
-// import Footer from '../../layout/Footer'
-import './style.scss';
-import { useParams } from 'react-router';
-import apiProdutos from '../../services/apiProdutos';
-import {useState, useEffect} from 'react'
+import Header from '../../layout/Header';
+import ProdutoMain from '../../layout/ProdutoMain'
+import Footer from '../../layout/Footer'
+
 
 
 function Produto() {
-    const {produtoId} = useParams();
-    const [produto, setProduto] = useState({});
-    useEffect(() => {
-        apiProdutos
-        .get(`/produto/${produtoId}`)
-        .then((response) => setProduto(response.data))
-        .catch((err) => {
-          console.error("ops! ocorreu um erro" + err);
-        })
-    }, [produto]);
     return (
         <>
-            <p>{produto.nome}</p>
-            <p>{produto.preco}</p>
-            <p>{produto.descricao}</p>
-            
-            {/* {<Header/>
-            <ProdutoMain/>
-            <Footer/>} */}
+            <Header />
+            <ProdutoMain />
+            <Footer />
         </>
     );
 }
