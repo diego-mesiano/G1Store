23b40117b994b314c15d783a4ff7f7068fbcfe74
William Lima <wilima010@gmail.com>
2021-12-10 16:15:50 -0300

    footer alinhado

--- g1store/src/layout/Footer/index.js
+++ g1store/src/layout/Footer/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/img/logo.png'
 function Footer() {
   return (
     <>
-      <footer>
+      <footer className="bg-primary">
         <h1><img src={logo}alt="Logo" width="150"/></h1>
         <h6>©Copyright 2021 g1.store - All Rights Reserved </h6>
       </footer>
