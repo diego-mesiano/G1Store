bb0a7cf98806539049a04326e44ab13d175f87ce
diego-mesiano <diego.mesiano1@gmail.com>
2021-12-10 03:32:46 -0300

    Ajustes na pagina sobre nós

--- g1store/src/layout/CarrinhoMain/index.js
+++ g1store/src/layout/CarrinhoMain/index.js
@@ -1,38 +1,40 @@
 import './style.scss';
-import {useContext} from 'react';
-import {CarrinhoContext} from '../../contexts/CarrinhoContext';
+import { useContext } from 'react';
+import { CarrinhoContext } from '../../contexts/CarrinhoContext';
+import React from 'react';
 
-function CarrinhoMain(){
-    const {produtosCarrinho} = useContext(CarrinhoContext)
-    
-    
-    
- return(
-     <>
-        {
-           produtosCarrinho.map((a)=>{
-                
-               return(
-                    <>
-                   <p>{a.nome}</p> 
-                   <p>{a.preco}</p>
-                   
-                   <p>---------------------</p>
-                   </>
-               )
-           })
-        
-            
-                
-                    
-                   
-                    
-                
-            
-        }
-        
-     </>
- );   
+
+function CarrinhoMain() {
+    const { produtosCarrinho } = useContext(CarrinhoContext)
+
+
+
+    return (
+        <>
+            {
+                produtosCarrinho.map((a) => {
+
+                    return (
+                        <React.Fragment key={a.nome}>
+                            <p>{a.nome}</p>
+                            <p>{a.preco}</p>
+
+                            <p>---------------------</p>
+                        </React.Fragment>
+                    )
+                })
+
+
+
+
+
+
+
+
+            }
+
+        </>
+    );
 }
 
 export default CarrinhoMain;
--- g1store/src/layout/SobreNosMain/index.js
+++ g1store/src/layout/SobreNosMain/index.js
@@ -1,102 +1,34 @@
 import './style.scss';
 import { Card } from 'react-bootstrap';
+import integrantes from './integrantes.json'
+import {jezi} from '../../assets/img/jezi.jfif'
 
 
 function SobreNosMain() {
 
   return (
     <>
-      <div class=" w-100 row g-3 justify-content-center my-5">
+      <div className=" w-100 row g-3 justify-content-center my-5">
 
-        <Card class="col-3 " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3" variant="top" src="https://media-exp1.licdn.com/dms/image/D4E35AQEldNK_KvWVOg/profile-framedphoto-shrink_800_800/0/1626628271702?e=1638986400&v=beta&t=IKSy8dD227fdKKQBDPEw8LpUX52JZkQzcuipVtgCoko" />
-          <Card.Body>
-            <Card.Title>Jeziel Oliveira de Paula</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/Jeziel1Oliveira2">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/jeziel-oliveira-de-paula-21b8211b3/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
-
-        <Card class="col-4 " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3"  variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQHVqPuwNZIoYA/profile-displayphoto-shrink_800_800/0/1627600430464?e=1644451200&v=beta&t=SYU2_FnPjuVC2d2GhFBbcNWXuyCKFfjd5rLfQr2mwwI" />
-          <Card.Body>
-            <Card.Title>Diego Mesiano</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/diego-mesiano/">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/diego-mesiano-87b680213/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
-
-        <Card class="col-4 " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3" variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQHe9_gU2W8ZzA/profile-displayphoto-shrink_800_800/0/1601911608133?e=1644451200&v=beta&t=SHniJ80MIm6MXCsLccspWH20P5QDGz0SsgA9HW2nCw8" />
-          <Card.Body>
-            <Card.Title>Nicole Stefany</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/NicoleStefany">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/nicole-stefany-da-silva-b3b73a185/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
-
-        <Card class="col-4 " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3" variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQFuxI06h0pRAw/profile-displayphoto-shrink_800_800/0/1578342809694?e=1644451200&v=beta&t=C9Gyx3X8UE2e8OVzA4InsCc00VJO4ParRAGNN57-dvM" />
-          <Card.Body>
-            <Card.Title>Gabriel Goak</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/g-oak">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/gabr1elcarvalho/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
-
-        <Card class="col-4  " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3" variant="top" src="https://media-exp1.licdn.com/dms/image/C4E03AQHWKb0nFpMXwQ/profile-displayphoto-shrink_800_800/0/1625524382053?e=1644451200&v=beta&t=9DbArv2_HCBoeYP5J53Ag6icTfrsdx7-cCLKrsmc2zQ" />
-          <Card.Body>
-            <Card.Title>Erik Kaiser</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/resyakKire">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/erik-kayser-b5228a212/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
-
-        <Card class="col-4 " style={{ width: '22rem' }}>
-          <Card.Img class="pt-3" variant="top" src="https://lh3.googleusercontent.com/WYH-ENAqZ_R07QOIXGQb1tkbP6Zw67Cqo_9vu81O_UIagrtfvrYuRFs6iKlSrLNi_8G5fQSzKkinE0ex47dFnToPt4hdWFqUPzyQWOMz5wGgxIsqZNlXzUn1wVL-GDU_x9MvUDcAQEBTFXPf1qRojm71944pE8G4MOd8nLv4lqXxQxfmcWgDz37f76Ju3mqMFlLK2mslCaDOL-FM4Xp9HNC6ERaTtESTXVU0SbNlYwg6TGG4qihIpxRu0Bd4npwFNghHKuw8duP-WaQvBoC8wLasCrrphpxEQ9rmdNPaOQihqW4FqBz8xyaMBRyIpQYphOWfQwAwFs-v1UXEgQ9trkBYQbwKsOJMDmp5dRqB29QvfxmnY4TcOKB97Gz-zGtnkjSr-X_X93bWPgzMatJ93CBUvVLf3oPkHvGY4wEiRsruPB-MtpOyszrmTVPypKyqLewC-6WQ45WEiYb1Lf2sPQcl4tGpVnk22bDwBcl9vY6xKHK8-vsDicR2OgfQKw-0RLQGAx-00pQK6Y7vOkrgzxXrCSS2-ZDNOkEqob7U0GaLw_KdfPQWATOWG5XjhWiuBOZlwNlPpnRw5nAqnsl5T8b6PUpr7HN_mgD9zQPAJJ5vL_-0s_Ph6sy4dNMD8llCIeqOSUrg9RoWdpaypY7fZLgia10fYLKUho0IcOG9-9x3gc9fHMU4AGGKLfyEGZc1n4oybSXlywjs6CDkjOOHml3m=w573-h578-no?authuser=0" />
-          <Card.Body>
-            <Card.Title>William Lima</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <Card.Body class="btn-group mb-2 mx-2 justify-content-center align-items-center">
-            <Card.Link class="btn btn-primary " href="https://github.com/WillCrill">Github</Card.Link>
-            <Card.Link class="btn btn-primary " href="https://www.linkedin.com/in/william-lima-826806216/">LinkedIn</Card.Link>
-          </Card.Body>
-        </Card>
+        {
+          integrantes.map(({ nome, foto, github, linkedin, bio }) => {
+            return (
+              <Card className="col-3 " style={{ width: '22rem' }}>
+                <Card.Img className="pt-3" variant="top" src={`${foto}`} />
+                <Card.Body>
+                  <Card.Title>{nome}</Card.Title>
+                  <Card.Text>
+                    {bio}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Body className="btn-group mb-2 mx-2 justify-content-center align-items-center">
+                  <Card.Link className="btn btn-primary" target="_blank" rel="noopener noreferrer" href={github}>Github</Card.Link>
+                  <Card.Link className="btn btn-primary" target="_blank" rel="noopener noreferrer" href={linkedin} >LinkedIn</Card.Link>
+                </Card.Body>
+              </Card>
+            )
+          })
+        }
       </div>
     </>
   );
